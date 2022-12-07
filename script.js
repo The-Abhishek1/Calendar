b5f0c106085bff4df0d16d49cef0a117ae229702
Abhishek <idiot63666@gmail.com>
2022-12-07 11:40:40 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,52 @@
+const currentDate = document.querySelector(".current-date")
+daysTag = document.querySelector(".days")
+prevNextIcon = document.querySelectorAll(".icons span")
+
+
+//getting new date ,current year and month
+let date = new Date(),
+    currYear = date.getFullYear(),
+    currMonth = date.getMonth();
+const months = ["Januray", "February", "March", "April", "May", "June", "July",
+    "August", "September", "October", "November", "December"
+];
+
+const renderCalendar = () => {
+    let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(), //getting first date of month
+        lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(), //getting last date  of month
+        lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(), //getting last day fo month
+        lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); //getting last date of month
+    let liTag = "";
+
+    for (let i = firstDayofMonth; i > 0; i--) {
+        liTag += `<li class="inactive">${lastDateofLastMonth-i + 1}</li>`
+    }
+
+    for (let i = 1; i <= lastDateofMonth; i++) {
+        let isToday = i === date.getDate() && currMonth === new Date().getMonth() &&
+            currYear === new Date().getFullYear() ? "active" : "";
+        liTag += `<li class="${isToday}">${i}</li>`
+    }
+    for (i = lastDayofMonth; i < 6; i++) {
+        liTag += `<li class="inactive">${i-lastDayofMonth + 1}</li>`
+    }
+
+    currentDate.innerHTML = `${months[currMonth]} ${currYear}`;
+    daysTag.innerHTML = liTag;
+}
+renderCalendar();
+
+prevNextIcon.forEach(icon => {
+    icon.addEventListener("click", () => {
+        currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
+        if (currMonth < 0 || currMonth > 11) {
+            date = new Date(currYear, currMonth);
+            currYear = date.getFullYear();
+            currMonth = date.getMonth();
+        } else {
+            date = new Date();
+        }
+        renderCalendar();
+    })
+
+});
